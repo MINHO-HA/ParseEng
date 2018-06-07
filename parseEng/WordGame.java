3fb74f42785ce056b7fccbd6194c62dc8d13bfc1
danny <gryb809@gmail.com>
2018-06-08 02:36:04 +0900

    WordGame 수정

--- parseEng/WordGame.java
+++ parseEng/WordGame.java
@@ -1,6 +1,9 @@
 package parseEng;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
 //타자게임기능 클래스
 public class WordGame {
 
@@ -13,15 +16,21 @@ public class WordGame {
 	
 	//워드게임 메소드 생성
 	public void wordgame() {
+		Set<String> keySet = Article.articleMap.keySet();
+		Iterator<String> iterator = keySet.iterator();
+		String choice = iterator.next();
+		
 		
 		//반복문으로 10문제 만들기
 		System.out.println();
 		for(int i=1; i<=10; i++) {
 			
 			// 기사내용 공백구분으로 리스 배열에 담은 데이터 Article클래스를 상속받아 꺼내오기.
-			random = (int)(Math.random()*ParseEng.wordList.size());
-			String words = ParseEng.wordList.get(random).wordname;
-			System.out.println(i+"번 문제 : "+ParseEng.wordList.get(random).wordname);
+			String key = choice;
+			ArrayList<Word> thisWordList = Article.articleMap.get(key);
+			random = (int)(Math.random()*thisWordList.size());
+			String words = thisWordList.get(random).wordname;
+			System.out.println(i+"번 문제 : "+thisWordList.get(random).wordname);
 			System.out.println(">");
 			
 			//꺼내온 단어로 입력게임시작
