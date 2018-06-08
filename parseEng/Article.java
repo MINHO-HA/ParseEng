8ab7d0f734204f44bd7da53d3c508362ea4b1c89
danny <gryb809@gmail.com>
2018-06-08 13:09:42 +0900

    TreeMap 변환 완료

--- parseEng/Article.java
+++ parseEng/Article.java
@@ -1,29 +1,37 @@
 package parseEng;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.NavigableMap;
+import java.util.Set;
+import java.util.TreeMap;
 
 
 public class Article {
 
-	public String title;
-	public String contents;
-	public static Map<String, ArrayList<Word>> articleMap = new HashMap<String, ArrayList<Word>>();
-		
-	/*// Map 생성
-				Map<String, String> map = new HashMap<String, String>();
-				
-				//객체 저장
-				map.put("ㅇ", "ㅇㅇ");
-				
-				//객체를 하나씩 처리
-				Set<String> keySet = map.keySet();
-				Iterator<String> keyIterator = keySet.iterator();
-				while(keyIterator.hasNext()) {
-					String key = keyIterator.next();
-					String value = map.get(key);
-					System.out.println(key+" : "+value);
-	//static ArrayList wordList;
-*/	
+	// 각 장문의 제목을 담는 String type 변수
+	public String title; 
+
+	// 장문 자체를 담는 String type 변수. contents에 담긴 문자열을 처리한 후에는 ArrayList<Word> type이 될 것.(ParseEng.java 참고)
+	public String contents; 
+
+	// 제목과 제목에 따른 분석 결과를 담을 정적변수 articleMap 선언
+	public static TreeMap<String, ArrayList<Word>> articleMap = new TreeMap<String, ArrayList<Word>>(); 
+	
+	
+	/****************************
+	 * Article의 리스트를 출력하는 메소드*
+	 ****************************/
+	public static void articleList() {
+		NavigableMap<String, ArrayList<Word>> descendingMap = articleMap.descendingMap();
+		Set<Map.Entry<String, ArrayList<Word>>> descendingEntrySet = descendingMap.entrySet();
+		int i=1;
+		System.out.println("======== Aritcle List ========");
+		for(Map.Entry<String, ArrayList<Word>> entry : descendingEntrySet) {
+			System.out.println(i + ". title : " + entry.getKey());
+			i++;
+		}
+		System.out.println();
+	}
+	
 }
--- parseEng/Word.java
+++ parseEng/Word.java
@@ -3,21 +3,19 @@ package parseEng;
 import java.util.ArrayList;
 
 public class Word implements Comparable<Word>{
-	String wordname; // 단어의 이름, 표제어, 단어 그 자체
-	int wordcount; // 단어가 등장한 횟수
-	static String meaning;
+	public String wordname; // 단어의 이름, 표제어, 단어 그 자체
+	public int wordcount; // 단어가 등장한 횟수
+	public String meaning; // 단어의 뜻
 	
+	// 생성자
 	Word(String wordname, int wordcount) {
 		this.wordname = wordname;
 		this.wordcount = wordcount;
 	}
-	
-	
-	
+
 	@Override
 	public int compareTo(Word word) {
 		return wordname.compareTo(word.wordname);
 	}
-	
 	
 }
